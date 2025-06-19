9f465c0652451f09aef3b8ad4881ad0f7abdb10c
Si Fad <sifayed00@gmail.com>
2025-06-19 21:22:17 +0600

    i added just a basic navbar

---
+++ src/pages/shared/ShiftifyLogo/ShiftifyLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../assets/logo.png';
+
+const ShiftifyLogo = () => {
+    return (
+        <div className='flex items-end'>
+            <img className='mb-1' src={logo} alt="" />
+            <h1 className='text-3xl -ml-[6px]'>Shiftify</h1>
+        </div>
+    );
+};
+
+export default ShiftifyLogo;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
+import Root from '../layouts/ShiftifyRoot';
+import Home from '../pages/home/Home/Home';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello World</div>,
+        Component: Root,
+        children: [
+            { index: true, Component: Home },
+        ],
     },
 ]);
 
