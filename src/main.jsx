1be75cf765b7dea2712b3dff00cd66c4895e7f7e
Si Fad <sifayed00@gmail.com>
2025-06-20 00:04:45 +0600

    i added a banner with Carousel

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { RouterProvider } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+   <div className='max-w-7xl mx-auto font-urbanist'>
+      <RouterProvider router={router} />
+   </div>
   </StrictMode>,
 )
--- src/pages/shared/Header/Navbar.jsx
+++ src/pages/shared/Header/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
+            <div className="navbar-start -ml-6">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -30,7 +30,7 @@ const Navbar = () => {
                     {navLinks}
                 </ul>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end -mr-2">
                 <a className="btn">Button</a>
             </div>
         </div>
