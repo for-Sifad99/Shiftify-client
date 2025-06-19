b27c7807b4343a559a0e556b5eb49abd455e039f
Si Fad <sifayed00@gmail.com>
2025-06-19 21:51:45 +0600

    i added a initial footer and added a brand logo.

--- src/layouts/ShiftifyRoot.jsx
+++ src/layouts/ShiftifyRoot.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../pages/shared/Header/Navbar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/pages/shared/ShiftifyLogo/ShiftifyLogo.jsx
+++ src/pages/shared/ShiftifyLogo/ShiftifyLogo.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/logo.png';
 const ShiftifyLogo = () => {
     return (
         <div className='flex items-end'>
-            <img className='mb-1' src={logo} alt="" />
+            <img className='mb-1' src={logo} alt="Shiftify website logo" />
             <h1 className='text-3xl -ml-[6px]'>Shiftify</h1>
         </div>
     );
