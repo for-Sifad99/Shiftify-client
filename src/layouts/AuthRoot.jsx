dd9089f52c18f42547eb3c2b08a9ad660a487de3
Si Fad <sifayed00@gmail.com>
2025-06-21 21:40:22 +0600

    i added a simple login form with react-form-hook.

--- src/layouts/AuthRoot.jsx
+++ src/layouts/AuthRoot.jsx
@@ -7,18 +7,20 @@ import { Outlet } from 'react-router';
 const AuthRoot = () => {
     return (
         <section className="min-h-screen">
-            <div className='py-10 w-fit'>
+            <div className='xl:px-0 sm:px-12 px-5 py-6 w-fit'>
                 <ShiftifyLogo />
             </div>
 
             <div className='hero'>
-                <div className="flex-1 hero-content flex-col lg:flex-row-reverse">
+                <div className="hero-content flex-col md:flex-row-reverse">
                     <img
                         src={authImage}
+                        className='flex-1 w-full max-w-[400px] sm:max-w-[500px] sm:w-[440px] md:w-[380px] lg:w-[40%] xl:w-[500px] '
                     />
-                    <div className='flex-1'>
+                    <div className="w-full sm:w-[100%] md:w-[420px] lg:w-[46%] xl:w-[400px] max-w-[330px]">
                         <Outlet />
                     </div>
+
                 </div>
             </div>
         </section>
--- src/pages/Authentication/AuthLogin/AuthLogin.jsx
+++ src/pages/Authentication/AuthLogin/AuthLogin.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AuthLogin = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    };
+    
     return <>
-        <h1 className="text-5xl font-bold">Welcome Back!</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="px-2">
+            <h1 className="sm:text-4xl text-3xl font-extrabold mb-1">Welcome Back!</h1>
+            <fieldset className="fieldset">
+                {/* email */}
+                <label className="label" >Email</label>
+                <input type="email" className="input" {...register('email')} placeholder="Email" />
+
+                {/* password */}
+                <label className="label">Password</label>
+                <input type="password" className="input" {...register('password', { required: true, minLength: 6 })} placeholder="Password" />
+
+                {/* forgot password */}
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-primary text-black mt-4">Login</button>
+            </fieldset>
+        </form>
     </>
 };
 
